30b0257423ebefe377216ce8b45b09c560903e42
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2015-04-25 13:22:50 -0500

    Separacion del codigo PHP del HTML

--- Administracion/index.php
+++ Administracion/index.php
@@ -1,121 +1,70 @@
 <?php
-  session_start();
   /**
     * Se adicion compatibilidad con variables globales en Off
     * @autor Jairo Losada 2009-05
     * @modificado por Cmauricio Parra
     * @licencia GNU/GPL V 3
     */
+  
+  session_start();
 
+  include ('../config.php');
+  include (SMARTY_TEMPLATE);
   foreach($_GET as $k=>$v) $$k=$v;
 
-  $krd          = $_SESSION["krd"];
-  $dependencia  = $_SESSION["dependencia"];
-  $usua_doc     = $_SESSION["usua_doc"];
-  $codusuario   = $_SESSION["codusuario"];
-  $tip3Nombre   = $_SESSION["tip3Nombre"];
-  $tip3desc     = $_SESSION["tip3desc"];
-  $tip3img      = $_SESSION["tip3img"];
+  $smarty = new Smarty();
+  $smarty->template_dir = TEMPLATE_DIR;
+  $smarty->compile_dir  = COMPILE_DIR;
+  $smarty->cache_dir    = CACHE_DIR;
+  
+  $krd          = $_SESSION['krd'];
+  $dependencia  = $_SESSION['dependencia'];
+  $usua_doc     = $_SESSION['usua_doc'];
+  $codusuario   = $_SESSION['codusuario'];
+  $tip3Nombre   = $_SESSION['tip3Nombre'];
+  $tip3desc     = $_SESSION['tip3desc'];
+  $tip3img      = $_SESSION['tip3img'];
   $ruta_raiz    = '..';
 
   $nomcarpeta   = (isset($_GET['carpeta']))? $_GET['carpeta'] : null;
   $tipo_carpt   = (isset($_GET['tipo_carpt']))? $_GET['tipo_carpt'] : null;
   $adodb_next_page = (isset($_GET['adodb_next_page']))? $_GET['adodb_next_page'] : null;
-
+  
+  // Sin acceso a usuario que no sea administrador
   if($_SESSION['usua_admin_sistema'] != 1)
-    die(include "$ruta_raiz/sinacceso.php");
+    die(include '../sinacceso.php');
 
-  if($krd!='AVALBUENA' AND $krd!='JZORA'){
-?>
-<html>
-<head>
-<title>Modulo Administraci&oacute;n</title>
-<link rel="stylesheet" href="../estilos/orfeo.css">
-</head>
-<body>
-<table width="71%" align="center" border="0" cellpadding="0" cellspacing="5" class="borde_tab">
-<tr bordercolor="#FFFFFF">
-	<td colspan="2" class="titulos4"><div align="center"><strong>M&Oacute;DULO DE ADMINISTRACI&Oacute;N</strong></div></td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="48%">
-		<a href='usuario/mnuUsuarios.php?krd=<?=$krd?>' target='mainFrame' class="vinculos">
-      1. USUARIOS Y PERFILES
-    </a>
-	</td>
-	<td align="center" class="listado2" width="48%">
-    <a href="tbasicas/adm_dependencias.php" class="vinculos" target="mainFrame">2. DEPENDENCIAS</a></td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="48%"> <a  href="tbasicas/adm_nohabiles.php" class="vinculos" target='mainFrame'>3. DIAS NO HABILES</a></td>
-	<td align="center" class="listado2" width="48%"><a href="../bdcompleme/tar_env.php" class="vinculos" target='mainFrame'>4. ENV&Iacute;O DE CORRESPONDENCIA</a> </td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_tsencillas.php" class="vinculos" target='mainFrame'>5. TABLAS SENCILLAS</a></td>
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_trad.php?krd=<?=$krd?>" class="vinculos" target='mainFrame'>6. TIPOS DE RADICACI&Oacute;N</a></td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_paises.php" class="vinculos" target='mainFrame'>7. PA&Iacute;SES</a></td>
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_dptos.php" class="vinculos" target='mainFrame'>8. DEPARTAMENTOS</a></td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_mcpios.php" class="vinculos" target='mainFrame'>9. MUNICIPIOS</a></td>
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_tarifas.php" class="vinculos" target='mainFrame'>10. TARIFAS</a></td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_contactos.php" class="vinculos" target='mainFrame'>11. CONTACTOS</a></td>
-	<td align="center" class="listado2" width="48%"><a href="tbasicas/adm_esp.php?krd=<?=$krd?>" class="vinculos" target='mainFrame'>12. ENTIDADES</a></td>
-</tr>
-</table>
-<br>
-<table width="71%" align="center" border="0" cellpadding="0" cellspacing="5" class="borde_tab">
-<tr bordercolor="#FFFFFF">
-	<td colspan="2" class="titulos4"><div align="center"><strong>M&Oacute;DULO DE AUDITORIA</strong></div></td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="48%">
-    <center>
-      <a href='auditoria/?krd=<?=$krd?>' target='mainFrame' class="vinculos">
-        ENTRAR
-      </a>
-    </center>
-	</td>
-</tr>
-</table>
-<br>
-<?php
-//tbasicas/adm_fenvios.php
- // MODULO OPCIONAL DE ADMINISTRACION DE FUNCIONARIOS Y ENTIDADES
- /* Por SuperSolidaria
-    Modifico y Adapto Jairo Losada 08/2009 */
-
-  $enlace_empresas = 'entidad/listaempresas.php?' .
-                      session_name() . '=' . session_id() .
-                      '&krd=' . $krd;
-
-  $enlace_funcionarios = 'usuario/listafuncionarios.php?' . 
+  $enlace_usuarios      = './usuario/mnuUsuarios.php' . '?krd=' . $krd;
+  $enlace_dependencias  = './tbasicas/adm_dependencias.php';
+  $enlace_dias          = './tbasicas/adm_nohabiles.php';
+  $enlace_envios        = '../bdcompleme/tar_env.php';
+  $enlace_tablas        = './tbasicas/adm_tsencillas.php';
+  $enlace_tipos         = './tbasicas/adm_trad.php' . '?krd=' . $krd;
+  $enlace_paises        = './tbasicas/adm_paises.php';
+  $enlace_deptos        = 'tbasicas/adm_dptos.php';
+  $enlace_entidades     = 'tbasicas/adm_esp.php' . '?krd=' . $krd;
+  $enlace_auditoria     = './auditoria/' . '?krd=' . $krd;
+  $enlace_empresas      = 'entidad/listaempresas.php?' .
+                          session_name() . '=' . session_id() .
+                          '&krd=' . $krd;
+  $enlace_funcionarios  = 'usuario/listafuncionarios.php?' . 
                           session_name() . '=' . session_id();
+  $enlace_esp           = 'tbasicas/adm_esp.php'. '?' . 'krd=' . $krd;
 
-  $enlace_esp = 'tbasicas/adm_esp.php?' .
-                  'krd=' . $krd ;
-}
+  $smarty->assign('ENLACE_USUARIOS', $enlace_usuarios);
+  $smarty->assign('ENLACE_DEPENDENCIAS', $enlace_dependencias);
+  $smarty->assign('ENLACE_DIAS', $enlace_dias);
+  $smarty->assign('ENLACE_ENVIOS', $enlace_envios);
+  $smarty->assign('ENLACE_TABLAS', $enlace_tablas);
+  $smarty->assign('ENLACE_TIPOS', $enlace_tipos);
+  $smarty->assign('ENLACE_PAISES', $enlace_paises);
+  $smarty->assign('ENLACE_DEPTOS', $enlace_deptos);
+  $smarty->assign('ENLACE_ENTIDADES', $enlace_entidades);
+  $smarty->assign('ENLACE_AUDITORIA', $enlace_auditoria);
+  $smarty->assign('ENLACE_EMPRESAS', $enlace_empresas);
+  $smarty->assign('ENLACE_FUNCIONARIOS', $enlace_funcionarios);
+  $smarty->assign('ENLACE_ESP', $enlace_esp);
+  $smarty->assign('ORFEO_URL', ORFEO_URL);
+
+  $smarty->display('index_administracion.html');
 ?>
-<table width="71%" align="center" border="0" cellpadding="0" cellspacing="5" class="borde_tab">
-<tr bordercolor="#FFFFFF">
-	<td colspan="3" class="titulos4" align="center">
-          Opcional x SES
-        </td>
-</tr>
-<tr bordercolor="#FFFFFF">
-	<td align="center" class="listado2" width="38%">
-		<a href="<?$enlace_empresas?>" target="mainFrame" class="vinculos">12. ENTIDADES  V.SES</a>
-	</td>
-	<td align="center" class="listado2" width="38%">
-	<a href="<?=$enlace_funcionarios?>" target='mainFrame'  class="vinculos">12.1 FUNCIONARIO - ENTIDAD</a>
-</td>
-<td align="center" class="listado2" width="38%">
-  <a href="<?=$enlace_esp?>" class="vinculos" target='mainFrame'>12. ENTIDADES</a></td>
-</tr>
-</table>
-</body>
-</html>
