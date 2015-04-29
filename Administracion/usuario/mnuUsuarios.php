cbe905c396308e16c44e97ca5bc54d8171adc108
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2015-04-29 08:57:13 -0500

    Separacion del codigo PHP y HTML del archivo mnuUsuario.php

--- Administracion/usuario/mnuUsuarios.php
+++ Administracion/usuario/mnuUsuarios.php
@@ -1,36 +1,22 @@
 <?php
-$ruta_raiz = "../..";
-session_start();
-if(!$_SESSION['dependencia'] or !$_SESSION['tpDepeRad']) include "$ruta_raiz/rec_session.php";
+  include ('../../config.php');
+  include (SMARTY_TEMPLATE);
+
+  $smarty = new Smarty();
+  $smarty->template_dir = TEMPLATE_DIR;
+  $smarty->compile_dir  = COMPILE_DIR;
+  $smarty->cache_dir    = CACHE_DIR;
+
+  session_start();
+  
+  if(!$_SESSION['dependencia'] or !$_SESSION['tpDepeRad'])
+    include '../../rec_session.php';
+  
+  $enlace_translado = 'traslado.php' . $phpsession .
+                      '&krd=' . $krd;
+  
+
+  $smarty->assign('ORFEO_URL', ORFEO_URL);
+  $smarty->assign('ENLACE_TRANSLADO', $enlace_translado);
+  $smarty->display('menu_usuarios.tpl');
 ?>
-<html>
-<head>
-<title>Documento sin t&iacute;tulo</title>
-<link rel="stylesheet" href="../../estilos/orfeo.css">
-</head>
-<body>
-<form name='frmMnuUsuarios' action='../formAdministracion.php' method="post">
-  <table width="32%" align="center" border="0" cellpadding="0" cellspacing="5" class="borde_tab">
-  <tr bordercolor="#FFFFFF">
-    <td colspan="2" class="titulos4"><div align="center"><strong>ADMINISTRACION DE USUARIOS Y PERFILES</strong></div></td>
-  </tr>
-  <tr bordercolor="#FFFFFF">
-    <td align="center" class="listado2" width="98%"><a href='crear.php?usModo=1' class="vinculos" target='mainFrame'>1. Crear Usuario</a></td>
-  </tr>
-  <tr bordercolor="#FFFFFF">
-    <td align="center" class="listado2" width="98%"><a href='cuerpoEdicion.php?usModo=2' class="vinculos" target='mainFrame'>2. Editar Usuario</a></td>
-  </tr>
-  <tr bordercolor="#FFFFFF">
-    <td align="center" class="listado2" width="98%"><a href='cuerpoConsulta.php' class="vinculos" target='mainFrame'>3. Consultar Usuario</a></td>
-  </tr>
-  <tr bordercolor="#FFFFFF">
-    <td align="center" class="listado2" width="98%"><a href="traslado.php?<?=$phpsession;?>&krd=<?=$krd;?>" class="vinculos" target='mainFrame'>4. Translado Usuario</a></td>
-  </tr>
-  <tr bordercolor="#FFFFFF">
-  	<td align="center" class="listado2">
-	<center><input align="middle" class="botones" type="submit" name="Submit" value="Cerrar"></center>
-	</td> </tr>
-</table>
-</form>
-</body>
-</html>
